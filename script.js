623ce0a84d58c8aa0ed69572eb76d65c31cd3590
mukuld86 <mukul.deshwal@outlook.com>
2025-06-29 09:50:12 +0530

    You ARE GAY!

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const moveDistance = 60;
 
 function buttonClicked(){
     alert("You are GAY!!!");
-    document.body.style.backgroundImage = "linear-gradient(90deg, red, orange, yellow, green, blue, indigo, violet)";
     let heading = document.createElement("H1");
     heading.innerText = "You are GAY!!!";
     document.body.appendChild(heading);
     heading.style.color = "black";
     heading.style.borderRadius = "10px";
+    document.body.style.backgroundImage = "linear-gradient(90deg, red, orange, yellow, green, blue, indigo, violet)";
 }
 
 
